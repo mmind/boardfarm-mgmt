6f390f003366fc3ec96a7d26639b425ff03aaeef
Heiko Stuebner <heiko@sntech.de>
2016-11-30 10:41:55 +0000

    sleep for a bit before retrying a telnet connection to ser2net
    This makes it easier to read the log as any information doesn't scroll away
    so easily.

--- telnet.sh
+++ telnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "/usr/bin/telnet localhost $1"
-bash -c "while /bin/true; do /usr/bin/telnet localhost $1; done"
+bash -c "while /bin/true; do /usr/bin/telnet localhost $1; sleep 2; done"
