852989ee741414b59a3d2392f235fd812c34b00b
Heiko Stuebner <heiko@sntech.de>
2023-04-15 01:28:22 +0200

    Frontend: add unfinished split-view to switch between console and graphics
    Of course this is still very much WIP.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- source/class/sn/boardfarm/view/desktop/BoardView.js
+++ source/class/sn/boardfarm/view/desktop/BoardView.js
@@ -8,29 +8,51 @@
  */
 
 qx.Class.define("sn.boardfarm.view.desktop.BoardView", {
-	extend : qx.ui.splitpane.Pane,
+//	extend : qx.ui.splitpane.Pane,
+	extend : qx.ui.tabview.TabView,
 
 	construct : function()
 	{
-		this.base(arguments, "vertical");
-		this.setDecorator(null);
+		this.base(arguments);
+		this.setContentPadding(0);
 
-	// Create the list view
-	this.__listView = new qx.ui.basic.Label("Simple text label");;
-	this.__listView.setHeight(150);
-	this.__listView.setDecorator("main");
+		this.__serial = new qx.ui.tabview.Page();
+		this.__serial.setLabel("Console");
+		this.__serial.setLayout(new qx.ui.layout.Dock());
+		this.add(this.__serial);
 
-		var scroll = new qx.ui.container.Scroll();
-		scroll.add(this.__listview);
-		this.add(scroll, 0);
-
+		this.__display = new qx.ui.tabview.Page();
+		this.__display.setLabel("Display");
+		this.__display.setLayout(new qx.ui.layout.Dock());
+		this.add(this.__display);
 
 		this.__vbox = new qx.ui.container.Composite(new qx.ui.layout.VBox());
-		this.add(this.__vbox, 1);
+		this.__serial.add(this.__vbox);
 
 		this.__xterms["_dummy"] = new sn.boardfarm.view.desktop.Xterm();
 		this.__xterms["_dummy"].setDecorator("main");
 		this.__vbox.add(this.__xterms["_dummy"], { flex : 1 });
+
+		this.__displayView = new sn.boardfarm.view.desktop.DisplayView();
+		this.__display.add(this.__displayView);
+
+
+//		this.__consolePane = new qx.ui.splitpane.Pane("vertical");
+//		this.__consolePane.setDecorator(null);
+//		this.base(arguments, "vertical");
+//		this.setDecorator(null);
+
+
+		// Create the list view
+//	this.__listView = new qx.ui.basic.Label("Simple text label");;
+//	this.__listView.setHeight(150);
+//	this.__listView.setDecorator("main");
+
+
+/*		var scroll = new qx.ui.container.Scroll();
+		scroll.add(this.__listview);
+		this.__serial.add(scroll, 0);*/
+
 
 	},
 
