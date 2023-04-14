c2c52d39e190a42ada4825b9a993e2bae2c4a30b
Heiko Stuebner <heiko@sntech.de>
2023-04-15 01:26:13 +0200

    Backend: add preliminary ffmpeg to LKV373a
    Add some unfinished code to LKV373a that allows it to start a ffmpeg
    instance from the received hdmi-data.
    
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>

--- source/class/sn/boardfarm/backend/mux/LKV373a.js
+++ source/class/sn/boardfarm/backend/mux/LKV373a.js
@@ -8,6 +8,7 @@
  */
 
 var mux = require("./AbstractMux");
+var spawn = require("child_process").spawn;
 
 /*
  * LKV373a is a cheap hdmi->network uni-/multicast encoder.
@@ -22,6 +23,38 @@ qx.Class.define("sn.boardfarm.backend.mux.LKV373a",
 	{
 		console.log("Mux: added LKV373a HDMI encoder " + ident);
 		this.base(arguments, ident, ctrl, pwr);
+
+		var mux = sn.boardfarm.backend.mux.Mux.getInstance();
+
+		mux.addMux(this.getIdent(), this);
+		mux.addSink(this.getIdent(), this);
+	},
+
+	events :
+	{
+		"stateChanged" : "qx.event.type.Data"
+	},
+
+	properties : {
+		/*
+		 * IP of the LKV373a
+		 */
+		deviceIp : { init : -1 },
+
+		/*
+		 * We use the mux-state to control ffmpeg streaming.
+		 * So it is of course off on creation.
+		 */
+		state : { init : 0, apply : "_applyState", event : "stateChanged" },
+
+		/*
+		 * Where to cast to.
+		 * Either the server 192.168.140.1 for the video stream to be
+		 * processed via ffserver and ffmpeg, or any other IP in the
+		 * local network (including the vpn) for the stream to be
+		 * displayed directly via vlc or so.
+		 */
+		castDestination : { init : -1, apply : "_applyCastDestination" },
 	},
 
 	members :
@@ -50,6 +83,68 @@ qx.Class.define("sn.boardfarm.backend.mux.LKV373a",
 				return;
 
 			throw "LKV373a only has one input port";
+		},
+
+		__ffmpeg : null,
+
+		/* Start or stop streaming to ffserver using ffmpeg */
+		_applyState : function(n, o)
+		{
+// ffmpeg -i "udp://@192.168.140.1:5004?fifo_size=524288" -r 10 -g 20 -c:v copy -an -f ffm http://127.0.0.1:8090/hdmi1.ffm
+			if (n) {
+				var dest = this.getCastDestination();
+
+				/* only start ffmpeg when we unicast to the server */
+				if (dest != "192.168.140.1")
+					return;
+
+				this.__ffmpeg = spawn('ffmpeg', [
+					'-i', '"udp://@192.168.140.1:5004?fifo_size=524288"',
+					'-r', '10',
+					'-g', '20',
+					'-c:v', 'copy',
+					'-an',
+					'-f', 'ffm',
+					'http://127.0.0.1:8090/hdmi1.ffm' ]);
+
+				this.__ffmpeg.stdout.on('data', (data) => {
+					console.log(`stdout: ${data}`);
+				});
+
+				this.__ffmpeg.stderr.on('data', (data) => {
+					console.log(`stderr: ${data}`);
+				});
+
+				this.__ffmpeg.on('close', (code) => {
+					console.log("child process exited with code ${code}");
+				});
+
+
+			} else {
+				if (!this.__ffmpeg)
+					return;
+
+				this.__ffmpeg.kill();
+			}
+		},
+
+		muxReadState : function()
+		{
+			/* empty */
+		},
+
+		_applyCastDestination : function(n, o)
+		{
+			var oldState = this.getState();
+
+			if (oldState)
+				this.setState(0);
+
+//curl "http://192.168.140.10/dev/info.cgi?action=streaminfo&udp=y&rtp=n&multicast=n&unicast=y&mcastaddr=192.168.178.56&port=5004"
+
+			if (oldState)
+				this.setState(1);
 		}
+
 	}
 });
